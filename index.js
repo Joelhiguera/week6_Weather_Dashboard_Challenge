b111896688c942072f9caaf4bcc3aa1290edc04d
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-08-09 16:25:54 -0600

    create a for loop that will create a 5 day forcast

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function currentWeather() {
       const { country } = data.sys;
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      const { dt_txt } = data.weather[0]
-      cityDisplay.textContent = dt_txt
+      // const { dt_txt } = data.weather[i]
+      // cityDisplay.textContent = dt_txt
 
       if (data.cod = data.message){
         // cityDisplay.textContent = data.message
@@ -59,38 +59,118 @@ function currentWeather() {
   
 }
 
+
+// need to create a for loop to run through the weather for the next 5 days
+
+// This is the original function. revert back when for loop does not work
+
+// function fiveDayForcast() {
+//   let cityValue = document.getElementById('city').value;
+//   let city = cityValue;
+//   const forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+//   fetch(forcastUrl)
+//     .then(function (response){
+//       return response.json()
+//     }).then(function (data){
+//       // day 1
+//       const forecastDate1 = document.getElementById('forecastDate1')
+//       const imageDay1 = document.getElementById('imageDay1')
+//       const tempDay1 = document.getElementById('tempDay1')
+//       const windDay1 = document.getElementById('windDay1')
+//       const humidityDay1 = document.getElementById('humidityDay1')
+      
+
+//       const { dt_txt } = data.list[i]
+//       const { icon } = data.list[i].weather[i]
+//       const { temp } = data.list[i].main
+//       const { speed } = data.list[i].wind
+//       const { humidity } = data.list[i].main
+
+      
+//       imageDay1.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+//       forecastDate1.textContent = dt_txt
+//       tempDay1.textContent = "Temp: " + temp + " °F"
+//       windDay1.textContent = "Wind: " + speed + " mph"
+//       humidityDay1.textContent = "Humidity: " + humidity + " %"
+
+    
+//       console.log(data)
+//     })
+// }
+
+
+
+
+
 function fiveDayForcast() {
   let cityValue = document.getElementById('city').value;
   let city = cityValue;
-  const forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+  const forcastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + apiKey + "&units=imperial";
 
   fetch(forcastUrl)
     .then(function (response){
       return response.json()
     }).then(function (data){
-      // day 1
-      const forecastDate1 = document.getElementById('forecastDate1')
-      const imageDay1 = document.getElementById('imageDay1')
-      const tempDay1 = document.getElementById('tempDay1')
-      const windDay1 = document.getElementById('windDay1')
-      const humidityDay1 = document.getElementById('humidityDay1')
+     console.log(data)
+     for (var i = 0; i < data.list.length; i++) {
+      const forecastContainer = document.getElementById('forecastContainer')
+      const daysDiv = document.createElement('div')
+      const forecastDate = document.createElement('h3')
+      const forecastTemp = document.createElement('p')
+      const forecastImg = document.createElement('img')
+      const forecastWind = document.createElement('p')
+      const forecastHumidity = document.createElement('p')
+      daysDiv.classList.add('days')
+
+      const { dt_txt } = data.list[i]
+      const { icon } = data.list[i].weather[0]
+      const { temp } = data.list[i].main
+      const { speed } = data.list[i].wind
+      const { humidity } = data.list[i].main
+
+      forecastDate.textContent = dt_txt
+      forecastTemp.textContent = "Temp: " + temp + " °F"
+      forecastWind.textContent = "Wind: " + speed + " mph"
+      forecastHumidity.textContent = "Humidity: " + humidity + " %"
+      forecastImg.setAttribute('src', "https://openweathermap.org/img/wn/" + icon + "@2x.png")
       
 
-      const { dt_txt } = data.list[0]
-      const { icon } = data.list[0].weather[0]
-      const { temp } = data.list[0].main
-      const { speed } = data.list[0].wind
-      const { humidity } = data.list[0].main
+
+
+     forecastContainer.appendChild(daysDiv);
+     daysDiv.appendChild(forecastDate);
+     daysDiv.appendChild(forecastImg);
+     daysDiv.appendChild(forecastTemp);
+     daysDiv.appendChild(forecastWind);
+     daysDiv.appendChild(forecastHumidity);
+
+     
+
+
+      // const forecastDate1 = document.getElementById('forecastDate1')
+      // const imageDay1 = document.getElementById('imageDay1')
+      // const tempDay1 = document.getElementById('tempDay1')
+      // const windDay1 = document.getElementById('windDay1')
+      // const humidityDay1 = document.getElementById('humidityDay1')
+      
+
+      // const { dt_txt } = data.list[i]
+      // const { icon } = data.list[i].weather[i]
+      // const { temp } = data.list[i].main
+      // const { speed } = data.list[i].wind
+      // const { humidity } = data.list[i].main
 
       
-      imageDay1.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-      forecastDate1.textContent = dt_txt
-      tempDay1.textContent = "Temp: " + temp + " °F"
-      windDay1.textContent = "Wind: " + speed + " mph"
-      humidityDay1.textContent = "Humidity: " + humidity + " %"
+      // imageDay1.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      // forecastDate1.textContent = dt_txt
+      // tempDay1.textContent = "Temp: " + temp + " °F"
+      // windDay1.textContent = "Wind: " + speed + " mph"
+      // humidityDay1.textContent = "Humidity: " + humidity + " %"
 
     
-      console.log(data)
+      
+     }
     })
 }
 
